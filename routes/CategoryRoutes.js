36e08781ae6286d504ed6c3144e9522fa5bf36dd
yasith <it19966922@my.sliit.lk>
2022-06-25 21:48:31 +0530

    find rooms by category

--- routes/CategoryRoutes.js
+++ routes/CategoryRoutes.js
@@ -1,9 +1,10 @@
 const KoaRouter = require('koa-router');
-const {addCategory, getAllCategories, getRoomsByCategory} = require("../controllers/CategoryController");
+const {addCategory, getAllCategories, getRoomsByCategory, getRoomsInEachCategory} = require("../controllers/CategoryController");
 const router = new KoaRouter({prefix: '/api/category'});
 
 router.post('/get-rooms-by-category',getRoomsByCategory);
 router.post('/add',addCategory);
 router.get('/get-all',getAllCategories);
+router.get('/get-categories-with-rooms',getRoomsInEachCategory);
 
 module.exports = router;
