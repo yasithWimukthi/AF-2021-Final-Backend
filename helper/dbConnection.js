ceca54f35d8bcb8033dd92ef7655fed4d55dfbb1
yasith <it19966922@my.sliit.lk>
2022-06-25 18:35:32 +0530

    create room model

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 
+/**
+ * Connect to MongoDB
+ */
 const dbConnect = () => {
     mongoose.connect(process.env.DB_CONNECTION, () => console.log('Connected to database'));
 }
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.use(cors());
 app.use(bodyParser());
 app.use(json());
 
+
 app.listen(3000, () => {
     dbConnect();
     console.log('Server is running on port 3000');
